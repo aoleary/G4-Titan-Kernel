93fdb1f04b997e3adc195176814696ade6fd8541
Tejun Heo <tj@kernel.org>
2013-06-13 19:58:38 -0700

    cgroup: update sane_behavior documentation
    f12dc02014 ("cgroup: mark "tasks" cgroup file as insane") and
    cc5943a781 ("cgroup: mark "notify_on_release" and "release_agent"
    cgroup files insane") forgot to update the changed behavior
    documentation in cgroup.h.  Update it.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -264,13 +264,21 @@ enum {
 	 *
 	 * - Remount is disallowed.
 	 *
+	 * - cpuset: tasks will be kept in empty cpusets when hotplug happens
+	 *   and take masks of ancestors with non-empty cpus/mems, instead of
+	 *   being moved to an ancestor.
+	 *
+	 * - cpuset: a task can be moved into an empty cpuset, and again it
+	 *   takes masks of ancestors.
+	 *
+	 * - "tasks" is removed.  Everything should be at process
+	 *   granularity.  Use "cgroup.procs" instead.
+	 *
+	 * - "release_agent" and "notify_on_release" are removed.
+	 *   Replacement notification mechanism will be implemented.
+	 *
 	 * - memcg: use_hierarchy is on by default and the cgroup file for
 	 *   the flag is not created.
-	 *
-	 * The followings are planned changes.
-	 *
-	 * - release_agent will be disallowed once replacement notification
-	 *   mechanism is implemented.
 	 */
 	CGRP_ROOT_SANE_BEHAVIOR	= (1 << 0),
 
