28958c635071be1074e6748937b2fb621d7a3c89
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-08-08 15:22:56 -0700

    driver core: bus_type: add drv_groups
    attribute groups are much more flexible than just a list of attributes,
    due to their support for visibility of the attributes, and binary
    attributes. Add drv_groups to struct bus_type which should be used
    instead of drv_attrs.
    
    drv_attrs will be removed from the structure soon.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/base.h
+++ drivers/base/base.h
@@ -120,6 +120,11 @@ static inline int driver_match_device(struct device_driver *drv,
 }
 extern bool driver_allows_async_probing(struct device_driver *drv);
 
+extern int driver_add_groups(struct device_driver *drv,
+			     const struct attribute_group **groups);
+extern void driver_remove_groups(struct device_driver *drv,
+				 const struct attribute_group **groups);
+
 extern int device_add_groups(struct device *dev,
 			     const struct attribute_group **groups);
 extern void device_remove_groups(struct device *dev,
