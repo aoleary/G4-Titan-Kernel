82a688b5118779a87d9b9089b728be888ec15b04
Steven Rostedt (Red Hat) <rostedt@goodmis.org>
2014-01-14 08:52:35 -0500

    tracing: Convert tracer->enabled to counter
    As tracers will soon be used by instances, the tracer enabled field
    needs to be converted to a counter instead of a boolean.
    This counter is protected by the trace_types_lock mutex.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -3963,7 +3963,7 @@ static void tracing_set_nop(struct trace_array *tr)
 	if (tr->current_trace == &nop_trace)
 		return;
 	
-	tr->current_trace->enabled = false;
+	tr->current_trace->enabled--;
 
 	if (tr->current_trace->reset)
 		tr->current_trace->reset(tr);
@@ -4009,7 +4009,7 @@ static int tracing_set_tracer(struct trace_array *tr, const char *buf)
 
 	trace_branch_disable();
 
-	tr->current_trace->enabled = false;
+	tr->current_trace->enabled--;
 
 	if (tr->current_trace->reset)
 		tr->current_trace->reset(tr);
@@ -4056,7 +4056,7 @@ static int tracing_set_tracer(struct trace_array *tr, const char *buf)
 	}
 
 	tr->current_trace = t;
-	tr->current_trace->enabled = true;
+	tr->current_trace->enabled++;
 	trace_branch_enable(tr);
  out:
 	mutex_unlock(&trace_types_lock);
