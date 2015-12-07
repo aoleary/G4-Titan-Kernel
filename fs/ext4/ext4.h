925f3a90f55c04dba01e56864dc7c13a5ed64752
Jan Kara <jack@suse.com>
2015-12-07 15:09:35 -0500

    ext4: provide ext4_issue_zeroout()
    Create new function ext4_issue_zeroout() to zeroout contiguous (both
    logically and physically) part of inode data. We will need to issue
    zeroout when extent structure is not readily available and this function
    will allow us to do it without making up fake extent structures.
    
    Change-Id: I5deb04b49d3ebdd1ac12f8bb950faf46d08f5d80
    Signed-off-by: Jan Kara <jack@suse.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Git-commit: 53085fac02d12fcd29a9cb074ec480ff0f77ae5c
    Git-repo: https://source.codeaurora.org/quic/la/kernel/msm-4.4
    [srkupp@codeaurora.org: Resolved minor conflict]
    Signed-off-by: Srinivasa Rao Kuppala <srkupp@codeaurora.org>

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -2426,6 +2426,8 @@ extern qsize_t *ext4_get_reserved_space(struct inode *inode);
 extern int ext4_get_projid(struct inode *inode, kprojid_t *projid);
 extern void ext4_da_update_reserve_space(struct inode *inode,
 					int used, int quota_claim);
+extern int ext4_issue_zeroout(struct inode *inode, ext4_lblk_t lblk,
+			      ext4_fsblk_t pblk, ext4_lblk_t len);
 
 /* indirect.c */
 extern int ext4_ind_map_blocks(handle_t *handle, struct inode *inode,
--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -3002,7 +3002,6 @@ static int ext4_ext_zeroout(struct inode *inode, struct ext4_extent *ex)
 {
 	ext4_fsblk_t ee_pblock;
 	unsigned int ee_len;
-	int ret;
 
 	ee_len    = ext4_ext_get_actual_len(ex);
 	ee_pblock = ext4_ext_pblock(ex);
