40a3bb67fe5a3796f85a2e68c1e5db7a9fd61905
Daniel Rosenberg <drosen@google.com>
2016-10-26 16:27:45 -0700

    vfs: Add permission2 for filesystems with per mount permissions
    This allows filesystems to use their mount private data to
    influence the permssions they return in permission2. It has
    been separated into a new call to avoid disrupting current
    permission users.
    
    Change-Id: I9d416e3b8b6eca84ef3e336bd2af89ddd51df6ca
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1603,7 +1603,7 @@ struct inode_operations {
 	struct dentry * (*lookup) (struct inode *,struct dentry *, unsigned int);
 	void * (*follow_link) (struct dentry *, struct nameidata *);
 	int (*permission) (struct inode *, int);
-        int (*permission2) (struct vfsmount *, struct inode *, int);
+	int (*permission2) (struct vfsmount *, struct inode *, int);
 	struct posix_acl * (*get_acl)(struct inode *, int);
 
 	int (*readlink) (struct dentry *, char __user *,int);
