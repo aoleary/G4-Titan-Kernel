9f67e945745ea0d2562ab2d8a579498cc20e86a7
Daniel Rosenberg <drosen@google.com>
2016-10-26 15:58:22 -0700

    vfs: Allow filesystems to access their private mount data
    Now we pass the vfsmount when mounting and remounting.
    This allows the filesystem to actually set up the mount
    specific data, although we can't quite do anything with
    it yet. show_options is expanded to include data that
    lives with the mount.
    
    To avoid changing existing filesystems, these have
    been added as new vfs functions.
    
    Change-Id: If80670bfad9f287abb8ac22457e1b034c9697097
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1666,12 +1666,12 @@ struct super_operations {
 	int (*statfs) (struct dentry *, struct kstatfs *);
 	int (*remount_fs) (struct super_block *, int *, char *);
 	int (*remount_fs2) (struct vfsmount *, struct super_block *, int *, char *);
-        void *(*clone_mnt_data) (void *);
-        void (*copy_mnt_data) (void *, void *);
+	void *(*clone_mnt_data) (void *);
+	void (*copy_mnt_data) (void *, void *);
 	void (*umount_begin) (struct super_block *);
 
 	int (*show_options)(struct seq_file *, struct dentry *);
-        int (*show_options2)(struct vfsmount *,struct seq_file *, struct dentry *);
+	int (*show_options2)(struct vfsmount *,struct seq_file *, struct dentry *);
 	int (*show_devname)(struct seq_file *, struct dentry *);
 	int (*show_path)(struct seq_file *, struct dentry *);
 	int (*show_stats)(struct seq_file *, struct dentry *);
@@ -1882,7 +1882,7 @@ struct file_system_type {
 		       const char *, void *);
 	struct dentry *(*mount2) (struct vfsmount *, struct file_system_type *, int,
 			       const char *, void *);
-        void *(*alloc_mnt_data) (void);
+	void *(*alloc_mnt_data) (void);
 	void (*kill_sb) (struct super_block *);
 	struct module *owner;
 	struct file_system_type * next;
