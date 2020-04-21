c9738aff339faeac1d3701c3444001e5d9d3673c
darkhz <kmachanwenw@gmail.com>
2020-04-21 10:30:27 +0530

    drivers: cpu-boost: Set initial boost to 0ms
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -44,7 +44,7 @@ static struct kthread_work input_boost_work;
 static unsigned int input_boost_enabled = 1;
 module_param(input_boost_enabled, uint, 0644);
 
-static unsigned int input_boost_ms = 2000;
+static unsigned int input_boost_ms = 0;
 module_param(input_boost_ms, uint, 0644);
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
