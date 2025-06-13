06086d03dfcd0a28ad43c58aa33365b2a0e1b18b
aoleary <seanm187@gmail.com>
2025-06-13 10:10:53 +0000

    Revert "fixup! mm: compaction: compact all zones when turning off screen : add missing linux/fb.h include"
    This reverts commit ebe9f53319551437f08841d3a111798d4c33fd6f.

--- mm/compaction.c
+++ mm/compaction.c
@@ -16,7 +16,6 @@
 #include <linux/sysfs.h>
 #include <linux/balloon_compaction.h>
 #include <linux/page-isolation.h>
-#include <linux/fb.h>
 #include <linux/kthread.h>
 #include <linux/freezer.h>
 #include <linux/psi.h>
